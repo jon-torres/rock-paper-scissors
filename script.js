2a0f10d36065fc8be44e641c7322929924a1f4b5
Jon Torres <jonathantorres10@gmail.com>
2021-11-16 12:50:59 -0300

    Added buttons functionality

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 'use strict';
 
+//DOM
+const btnRock = document.querySelector('.btn--rock');
+const btnPaper = document.querySelector('.btn--paper');
+const btnScissors = document.querySelector('.btn--scissors');
+
+// btnRock.textContent = 'rock';
+
+let playerSelection;
+
 // Initial values for the scores
 let playerScore = 0;
 let computerScore = 0;
 
 // The computer picks a random move between the items in the array.
-const computerPlay = function() {
-  const moves = [
-    'rock',
-    'paper',
-    'scissors'
-  ];
+const computerPlay = function () {
+  const moves = ['rock', 'paper', 'scissors'];
   return moves[Math.floor(Math.random() * moves.length)];
 };
 
 //It plays a round of the game and update the score values based on how the round went.
-const playRound = function() {
+const playRound = function (event) {
   let roundResult = '';
-  let playerSelection = prompt('Make your move and pick paper, rock or scissors!').toLowerCase();
+  playerSelection = event.target.innerText;
+  console.log(event.target.innerText);
+
   const computerSelection = computerPlay();
 
   // Tie
@@ -51,7 +58,7 @@ const playRound = function() {
 
     console.log(`You picked ${playerSelection} and the computer picked ${computerSelection}.
     ${roundResult}`);
-    
+
     // Basic "error checking".
   } else {
     console.log(`Please, enter rock, paper or scissors.`);
@@ -59,18 +66,22 @@ const playRound = function() {
 };
 
 // Plays 5 rounds of rock, paper and scissors and print the final results.
-const playFiveRounds = function() {
-  for (let i = 1; i <= 5; i++) {
-    playRound();
-  }
+// const playFiveRounds = function() {
+//   for (let i = 1; i <= 5; i++) {
+//     playRound();
+//   }
 
-  if (playerScore === computerScore) {
-    console.log(`It's a Tie! \n The final score is ${playerScore} vs. ${computerScore}.`);
-  } else if (playerScore < computerScore) {
-    console.log(`The computer won. Try again! \n The final score is ${playerScore} vs. ${computerScore}.`);
-  } else {
-    console.log(`Congratulations! You won! \n The final score is ${playerScore} vs. ${computerScore}.`);
-  }
-};
+//   if (playerScore === computerScore) {
+//     console.log(`It's a Tie! \n The final score is ${playerScore} vs. ${computerScore}.`);
+//   } else if (playerScore < computerScore) {
+//     console.log(`The computer won. Try again! \n The final score is ${playerScore} vs. ${computerScore}.`);
+//   } else {
+//     console.log(`Congratulations! You won! \n The final score is ${playerScore} vs. ${computerScore}.`);
+//   }
+// };
 
-playFiveRounds();
+// playFiveRounds();
+
+btnRock.addEventListener('click', playRound);
+btnPaper.addEventListener('click', playRound);
+btnScissors.addEventListener('click', playRound);
