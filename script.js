c128866308966dd5cfa4b8e9eabc6701d498a937
Jon Torres <jonathantorres10@gmail.com>
2021-11-17 09:24:33 -0300

    latest commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use strict';
 
 //DOM
+const container = document.querySelector('#container');
 const btnRock = document.querySelector('.btn--rock');
 const btnPaper = document.querySelector('.btn--paper');
 const btnScissors = document.querySelector('.btn--scissors');
 
-// btnRock.textContent = 'rock';
+let pResults = document.createElement('p');
+pResults.classList.add('pResults');
 
 let playerSelection;
+let playing = true;
 
 // Initial values for the scores
 let playerScore = 0;
@@ -21,54 +24,58 @@ const computerPlay = function () {
 
 //It plays a round of the game and update the score values based on how the round went.
 const playRound = function (event) {
-  let roundResult = '';
-  if (event.target.className === 'btn btn--rock') {
-    playerSelection = 'rock';
-  } else if (event.target.className === 'btn btn--paper') {
-    playerSelection = 'paper';
-  } else {
-    playerSelection = 'scissors';
+  if (playing) {
+    let roundResult = '';
+    if (event.target === btnRock) {
+      playerSelection = 'rock';
+    } else if (event.target === btnPaper) {
+      playerSelection = 'paper';
+    } else {
+      playerSelection = 'scissors';
+    }
+
+    const computerSelection = computerPlay();
+
+    // Tie
+    if (playerSelection === computerSelection) {
+      roundResult = `Tie! No one wins this round. The current score is ${playerScore} vs. ${computerScore}`;
+
+      pResults.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}.
+    ${roundResult}`;
+      container.appendChild(pResults);
+
+      //Player win conditions.
+    } else if (
+      (playerSelection === 'scissors' && computerSelection === 'paper') ||
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+      playerScore++;
+
+      roundResult = `You won this round! The score is now ${playerScore} vs. ${computerScore}`;
+
+      pResults.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}.
+    ${roundResult}`;
+      container.appendChild(pResults);
+
+      // Machine win conditions.
+    } else {
+      computerScore++;
+      roundResult = `You lose this round! The score is now ${playerScore} vs. ${computerScore}`;
+
+      pResults.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}.
+    ${roundResult}`;
+      container.appendChild(pResults);
+    }
   }
-  console.log(event.target.className);
-  console.log(event.target);
-
-  const computerSelection = computerPlay();
-
-  // Tie
-  if (playerSelection === computerSelection) {
-    roundResult = `Tie! No one wins this round. The current score is ${playerScore} vs. ${computerScore}`;
-
-    console.log(`You picked ${playerSelection} and the computer picked ${computerSelection}.
-    ${roundResult}`);
-
-    //Player win conditions.
-  } else if (
-    (playerSelection === 'scissors' && computerSelection === 'paper') ||
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock')
-  ) {
-    playerScore++;
-
-    roundResult = `You won this round! The score is now ${playerScore} vs. ${computerScore}`;
-
-    console.log(`You picked ${playerSelection} and the computer picked ${computerSelection}.
-    ${roundResult}`);
-
-    // Machine win conditions.
-  } else if (
-    (computerSelection === 'scissors' && playerSelection === 'paper') ||
-    (computerSelection === 'rock' && playerSelection === 'scissors') ||
-    (computerSelection === 'paper' && playerSelection === 'rock')
-  ) {
-    computerScore++;
-    roundResult = `You lose this round! The score is now ${playerScore} vs. ${computerScore}`;
-
-    console.log(`You picked ${playerSelection} and the computer picked ${computerSelection}.
-    ${roundResult}`);
-
-    // Basic "error checking".
-  } else {
-    console.log(`Please, enter rock, paper or scissors.`);
+  if (computerScore === 5) {
+    pResults.textContent = `The computer won. Try again! \n The final score is ${playerScore} vs. ${computerScore}.`;
+    container.appendChild(pResults);
+    playing = false;
+  } else if (playerScore === 5) {
+    pResults.textContent = `Congratulations! You won! \n The final score is ${playerScore} vs. ${computerScore}.`;
+    container.appendChild(pResults);
+    playing = false;
   }
 };
 
